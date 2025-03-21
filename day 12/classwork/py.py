8c2d64f410a2f396045c4172279d7d246c27d657
sabagigani <giganisaba54@gmail.com>
2025-03-21 21:33:53 +0400

    yaya commit

---
+++ day 12/classwork/py.py
@@ -0,0 +1,14 @@
+print(10<2 and 200>2000)
+print(12==12 and 200==200)
+print(12>10 or 200>2000)
+print(2000<= 90 and 200 <= 200)
+print(200 >= 20 or 20>=20)
+print(20!=20 and 10 != 9)
+name = "True and False"
+print(name)
+print(True and False or False or True and False)#გამოიტანა False
+print(False or True and False or True)#TRUE
+print(False and True or True and False or True)#TRUE
+print((True and False) or (False or True and True))#TRUE
+print(True and True or False and False or True)#TRUE
+print((False or True) and (True or False and True))#TRUE
---
+++ day 12/homework/x.py
@@ -0,0 +1,6 @@
+print(1 != 2)
+print(1 == 1)
+print(True and 1 != 1)
+x = 1 == 1
+p = 2 != 3
+print(x and p)
