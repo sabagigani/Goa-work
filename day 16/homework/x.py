d9ac0fbf01f1327300e6f2c5049234236348f148
sabagigani <giganisaba54@gmail.com>
2025-04-09 19:02:03 +0400

    gitooo commit

---
+++ day 16/homework/x.py
@@ -0,0 +1,9 @@
+# debogging არის შეცდომის შესწორება
+num1=input("enter you're lottory number to know if you win:")
+print(type(num1))
+# it is an str
+# casting არის მონაცემთა ტიპის შეცვლა 
+# თუ გვინდა მონაცემთა ტიპი გადავიტანოთ int
+# თუ გვინდა მონაცემთა ტიპი გადავიტანოთ float
+# თუ გვინდა მონაცემთა ტიპი გადავიტანოთ str
+# თუ გვინდა მონაცემთა ტიპი გადავიტანოთ bool
