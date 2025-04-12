982cc347b166d120a29df2441a954a8f14f24512
sabagigani <giganisaba54@gmail.com>
2025-04-12 10:37:22 +0400

    commit commit

---
+++ day 18/classwork/x/py.py
@@ -0,0 +1,14 @@
+for i in range(1,10,2):
+              
+               
+    print(i)
+# step არის ნაბიგი სადაც შეიყვანთ რამდენი ნაბიგუთი უნდა გაიროს
+# range არის მითითებული ციფრის დათვლა 0-მდე იმ მითითებულ ნომრამდე 
+for i in range(1,100 +1):
+    print(i,"GOA BEST!")
+
+for i in range(10):
+    c=input("enter your number ")
+    print(c)        
+  
+  
---
+++ day 18/homework/x/x.py
@@ -0,0 +1,6 @@
+for i in range(1,1000 +1):
+    print("GOA BEST")
+x=int(input("enter random number "))   
+for i in range(x):
+    print("GOA BEST")
+    
