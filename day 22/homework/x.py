dda2cd50886d03460cf6c954633d6561f4ec3b99
sabagigani <giganisaba54@gmail.com>
2025-04-26 12:12:45 +0400

    commit commit

---
+++ day 22/homework/x.py
@@ -0,0 +1,45 @@
+x=int(input ("enter number "))
+for i in range(0,x +1):
+    print(i)
+w=int(input ("enter number "))
+v=int(input ("enter number "))
+for i in range(0,x +w +1):
+    print(i)
+e=int(input ("enter number "))
+
+
+
+for i in range(0,e +1,3):
+    print(i)
+n=int(input ("enter number "))
+for i in range(0,n +1,4):
+    print(i)
+p=int(input ("enter number "))
+a=int(input ("enter number "))
+for i in range(0,p + a,3):
+    print(i)
+i=input("type your real name ")
+while i != "guram":
+    input("type your real name ")
+    print(i)
+r = int(input("type a randem numbeer"))
+while r != 100:
+    r = int(input("type a randem numbeer"))
+    print(r)
+u = int(input("type a randem numbeer"))
+while u != 5:
+    u = int(input("type a randem numbeer"))
+    print("guram teacher")
+password=9877
+wrong=0
+password_gfd=int(input("enter password"))
+while password_gfd != password:
+    password_gfd=int(input("enter password"))
+    wrong += 1
+print(wrong,"მცდელობები")   
+    
+
+
+
+
+    
